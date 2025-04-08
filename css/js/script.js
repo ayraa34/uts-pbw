2c2bfb0f6b690bd01fa826694a167d91c22d394b
ayraa34 <ggg446756@gmail.com>
2025-04-08 20:30:42 +0700

    upload tugas uts

---
+++ css/js/script.js
@@ -0,0 +1,7 @@
+let typed = new Typed(".teks", {
+    strings: ["Sedang Mengerjakan Projek Uts", "Sistem Informasi "],
+    typeSpeed: 60,
+    backSpeed: 60,
+    backDelay: 1000,
+    loop: true,
+  });
